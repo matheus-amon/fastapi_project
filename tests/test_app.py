18749a4530a44760e4d9326a6d78d959df729e56
matheus-amon <matheus.amon@outlook.com>
2025-08-05 14:02:27 -0300

    first commit

---
+++ tests/test_app.py
@@ -0,0 +1,12 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from fastzero.app import app
+
+
+def test_root_return_hello_world():
+    client = TestClient(app)
+    response = client.get('/')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'Olá Mundo!'}
